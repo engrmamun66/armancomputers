476230fb51679076f4490ae0983f59beab8635aa
Al-Mamun <mamun@leapinglogic.com>
2026-08-10 23:20:31 +0600

    Allowed host and cors

--- laravel-app/vite.config.js
+++ laravel-app/vite.config.js
@@ -19,6 +19,8 @@ export default defineConfig({
         },
     },
     server: {
+        host: true,
+        cors: true,
         watch: {
             ignored: ['**/storage/framework/views/**'],
         },
